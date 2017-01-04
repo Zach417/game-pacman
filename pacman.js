f082490d98b774aa79163d51fc5a30bd59453471
Zach Allen <zallen@pension-consultants.com>
2017-01-03 21:39:52 -0600

    refactored movement, draw, and position mechanics

--- pacman.js
+++ pacman.js
@@ -1,12 +1,12 @@
 function Pacman () {
   this.dir = createVector(0,0);
-  this.pos = createVector(7.5*scl, 15.5*scl);
+  this.pos = createVector(7, 15);
 
   this.checkPortal = function () {
-    if (this.pos.x <= -20 && this.pos.y == 380) {
-      this.pos.x = 580;
-    } else if (this.pos.x >= 600 && this.pos.y == 380) {
-      this.pos.x = 20;
+    if (this.pos.x <= -1 && this.pos.y == 9) {
+      this.pos.x = 14;
+    } else if (this.pos.x >= 15 && this.pos.y == 9) {
+      this.pos.x = 0;
     }
   }
 
@@ -16,54 +16,24 @@ function Pacman () {
   }
 
   this.update = function () {
-    if (this.dir.x == 1) {
-      this.right();
-    } else if (this.dir.x == -1) {
-      this.left();
-    } else if (this.dir.y == 1) {
-      this.down();
-    } else if (this.dir.y == -1) {
-      this.up();
-    }
+    this.checkCollision();
+    this.pos.add(this.dir);
+    this.checkPortal();
   }
 
-  this.left = function () {
-    if (!maze.contains(this.pos.x - scl, this.pos.y)) {
-      this.pos.x -= scl;
-    } else {
-      this.setDir(0,0);
-    }
-  }
-
-  this.right = function () {
-    if (!maze.contains(this.pos.x + scl, this.pos.y)) {
-      this.pos.x += scl;
-    } else {
-      this.setDir(0,0);
-    }
-  }
-
-  this.up = function () {
-    if (!maze.contains(this.pos.x, this.pos.y - scl)) {
-      this.pos.y -= scl;
-    } else {
-      this.setDir(0,0);
-    }
-  }
-
-  this.down = function () {
-    if (!maze.contains(this.pos.x, this.pos.y + scl)) {
-      this.pos.y += scl;
-    } else {
+  this.checkCollision = function () {
+    var newX = this.pos.x + this.dir.x;
+    var newY = this.pos.y + this.dir.y;
+    if (maze.contains(newX, newY)) {
       this.setDir(0,0);
     }
   }
 
   this.show = function () {
     push();
-    strokeWeight(30);
+    strokeWeight(3/4*scl);
     stroke('yellow');
-    point(this.pos.x, this.pos.y);
+    point((this.pos.x*scl)+floor(scl/2), (this.pos.y*scl)+floor(scl/2));
     pop();
   }
 }
