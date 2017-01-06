9ad6c4a4c90a6f248d5371518d82ce8aec842293
Zach Allen <zallen@pension-consultants.com>
2017-01-05 21:49:09 -0600

    added mega food

--- food.js
+++ food.js
@@ -1,10 +1,17 @@
 function Food (x,y) {
   this.x = x;
   this.y = y;
+  this.strokeWeight = 1/10*scl;
+  this.type = "normal";
+
+  this.mega = function () {
+    this.strokeWeight = 4/10*scl;
+    this.type = "mega";
+  }
 
   this.show = function () {
     push();
-    strokeWeight(1/10*scl);
+    strokeWeight(this.strokeWeight);
     stroke('white');
     point((this.x*scl)+floor(scl/2), (this.y*scl)+floor(scl/2));
     pop();
